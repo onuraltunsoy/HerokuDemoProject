32e224eeb80abdf1aa6b6f77d27387c701f99dc1
onur Altunsoy <onuraltunsoy28@gmail.com>
2019-07-17 00:31:45 +0300

    added tureng

--- src/main/java/com/ilkaygunel/restapi/GetWebService.java
+++ src/main/java/com/ilkaygunel/restapi/GetWebService.java
@@ -3,10 +3,17 @@ package com.ilkaygunel.restapi;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+
 import com.ilkaygunel.pojo.FormulaOneDriver;
+import com.ilkaygunel.tureng.Results;
+import com.ilkaygunel.tureng.Tureng;
 
 @RestController
 public class GetWebService {
@@ -22,8 +29,18 @@ public class GetWebService {
 		FormulaOneDriver kubica = new FormulaOneDriver("Robert", "Kubica");
 		driverList.add(kubica);
 		FormulaOneDriver test = new FormulaOneDriver("testName", "testLastName");
-		driverList.add(kubica);
+		driverList.add(test);
 
 		return driverList;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET, value = "/tureng")
+	public ResponseEntity<Results> getTureng(@RequestParam("name")String name) {
+		
+		Tureng tureng = new Tureng();
+		Results results = tureng.translate(name); 
+		///results.print();
+		
+		return ResponseEntity.ok(results);
 	}
 }
